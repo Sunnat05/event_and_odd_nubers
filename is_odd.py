abbae24d9d942a25f40e640df584d196431001d1
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-06-22 17:40:18 +0500

    nth

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,4 @@
-#Create a variable "number" and assign it a integer value.
-
+num=int(input()) #Create a variable "number" and assign it a integer value.
+a=num%2
+print(a)
 #Print 1 if the number is odd, otherwise print 0.
