7e2d01105e6f222ccbc8874107867ef025617d98
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-06-22 20:46:44 +0500

    nth

--- number_even.py
+++ number_even.py
@@ -1,11 +1,7 @@
 A=int(input()) #A four-digit integer is given. Find the number of even digits in it.
-a=A%10
-b=A//10%10 #Create a variable "var_int" and assign it a four-digit integer value.
-c=A//100%10
-d=A//1000 #Print the number of even digits in the variable "var_int".
-x1=abs(a%2-1)
-x2=abs(b%2-1)
-x3=abs(c%2-1)
-x4=abs(d%2-1)
-sum=x1+x2+x3+x4
-print(sum)
+a=abs(A%10%2-1)
+b=abs(A//10%10%2-1) #Create a variable "var_int" and assign it a four-digit integer value.
+c=abs(A//100%10%2-1)
+d=abs(A//1000%2-1) #Print the number of even digits in the variable "var_int".
+var_int=a+b+c+d
+print(var_int)
