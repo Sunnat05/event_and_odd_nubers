f0541c06efd8433a708d8e1f06fc78ae4c14868c
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-06-22 21:50:40 +0500

    nth

--- is_even.py
+++ is_even.py
@@ -1,4 +1,4 @@
-num=int(input()) #Create a variable "number" and assign it a integer value.
+number=int(input()) #Create a variable "number" and assign it a integer value.
 a=abs(num%2-1)
 print(a)
 #Print 1 if the number is even, otherwise print 0.
