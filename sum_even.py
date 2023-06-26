b7b7b1c17ecfddbf95446c2f0e6737c57c8eeb2b
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-06-27 00:39:48 +0500

    nth

--- sum_even.py
+++ sum_even.py
@@ -1,7 +1,10 @@
-#A four-digit integer is given. Find the sum of even digits in it.
-
-#Create a variable "var_int" and assign it a four-digit integer value.
-
-#Create a variable "sum_even" and assign it 0.
-
-#Find the sum of the even digits in the variable "var_int".
+var_int=int(input())#A four-digit integer is given. Find the sum of even digits in it.
+sum_even=0
+sum_even+=(var_int%10)*(1-var_int%2)#Create a variable "var_int" and assign it a four-digit integer value.
+var_int=var_int//10
+sum_even+=(var_int%10)*(1-var_int%2)#Create a variable "sum_even" and assign it 0.
+var_int//=10
+sum_even+=(var_int%10)*(1-var_int%2)#Find the sum of the even digits in the variable "var_int".
+var_int//=10
+sum_even+=(var_int%10)*(1-var_int%2)
+print(sum_even)
